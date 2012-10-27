d3772d868097759217ebfc2d11da185591fea7f8
kevin.heinicke <kevin.heinicke@udo.edu>
2012-10-27 12:25:50 +0200

    V406 mit ersten Daten hinzugefuegt

---
+++ V406_Beugung/graph_3.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x, y = np.genfromtxt('daten_mr_3.txt', unpack=True)
+# Datensatz in x und y einlesen
+
+x -= 25
+# x um 25 nach links verschieben (Maximum zentrieren)
+
+x_ticks = range(-25, 26, 5)
+# Schritte fuer x-Skalierung festlegen
+
+plt.plot(x, y, 'kx')
+plt.title('Messreihe 1')
+plt.xlabel('x [mm]')
+plt.ylabel('I [nA]')
+plt.xticks(x_ticks)
+plt.grid(b = True, which = "major")
+plt.show()
