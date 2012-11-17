b3e7c06614600122f56b2b949b4c0e3a36854c9d
bixel <kevin.heinicke@gmail.com>
2012-11-17 18:17:42 +0100

    einmal linear und einmal nichtlinear

--- V702_Aktivierung/data/fit_linear.py
+++ V702_Aktivierung/data/fit_linear.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 from scipy.optimize import curve_fit
+import datetime as dt
 
-x, y = np.genfromtxt('Rhodium_12s.txt', unpack = True)
-
-data = 'rhodium'
+data = 'indium'
 
 if data == 'rhodium':
 	# Graph fuer Rhodium
+	x, y = np.genfromtxt('Rhodium_12s.txt', unpack = True)
+
 	x *= 12
 
 	y -= 256 / 900 * 12
@@ -28,7 +29,7 @@ if data == 'rhodium':
 	m_vorschlag = .001
 	n_0_vorschlag = 1.0
 
-	funk = lambda t, m, n_0: n_0 * np.e**(- m * t)
+	funk = lambda t, m, n_0: n_0 * (1 - np.e ** (-m * 12)) * np.e**(- m * t)
 
 	vor_werte = np.array([m_vorschlag, n_0_vorschlag])
 	p, cov = curve_fit(funk, x1, y1, p0 = vor_werte, maxfev = 1000)
@@ -52,33 +53,46 @@ if data == 'rhodium':
 	plt.plot(x_theorie, funk(x_theorie, p2[0], p2[1]), 'b-')
 	plt.errorbar(x2, y2, yerr = np.sqrt(y2), fmt = 'kx')
 
+	t_halb1 = np.log(2) / p[0]
+	t_halb2 = np.log(2) / p2[0]
+
+	print('Halbwertszeiten 1 und 2: ' + str(t_halb1) + '; ' + str(t_halb2))
+
 elif data == 'indium':
 	# Graph fuer Indium
+	x, y = np.genfromtxt('Indium_240s.txt', unpack = True)
+
 	x *= 240
 
 	y -= 256 / 900 * 240
+	y = np.log(y)
 
 	y_error = np.sqrt(y)
 	#y_error = np.log(y)
 	#y = np.log(y)
 
-	x_theorie = np.arange(0, 15*240.001, 1)
+	x_theorie = np.arange(0, 15 * 240.001, 1)
 
 	plt.xlabel(r'$t\,[\mathrm{s}]$')
 	plt.ylabel(r'$\mathrm{Zerf\"alle}\,[1 / 240\mathrm{s}]$')
 
-	m_vorschlag = .01
+	m_vorschlag = 2.127e-4
 	n_0_vorschlag = 1.0
 	vor_werte = np.array([m_vorschlag, n_0_vorschlag])
 
-	funk = lambda t, m, n_0: n_0 * np.e**(- m * t)
+	funk = lambda t, m, n_0: n_0 * (1 - np.e ** (-m * 240)) * np.e**(- m * t)
 
 	p, cov = curve_fit(funk, x, y, p0 = vor_werte, maxfev = 5000)
 	print('Erster Teil: ')
 	print(p)
 	print(cov)
 	plt.plot(x_theorie, funk(x_theorie, p[0], p[1]), 'r-')
-	plt.errorbar(x, y, yerr = y_error, fmt = 'x')
 
-plt.savefig('../img/graph_indium.png')
+	plt.errorbar(x, y, yerr = np.sqrt((y_error ** 2) + 256 / 900 * 240), fmt = 'kx')
+
+	t_halb = np.log(2) / p[0]
+
+	print('Halbwertszeit: ' + str(t_halb) + 's = ' + str(dt.timedelta(seconds = t_halb)))
+
+plt.savefig('../img/graph_' + data + '_linearisiert.png')
 plt.clf()
