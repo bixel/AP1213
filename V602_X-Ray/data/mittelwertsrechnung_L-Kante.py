a775d29c90407a7c9b9b547dbb510642de57b3cc
bixel <kevin.heinicke@gmail.com>
2012-11-22 10:38:08 +0100

    fehler mit krasser formel fuer sigma

--- V602_X-Ray/data/mittelwertsrechnung_L-Kante.py
+++ V602_X-Ray/data/mittelwertsrechnung_L-Kante.py
@@ -4,24 +4,25 @@ import scipy as sp
 from scipy.optimize import curve_fit
 
 rydberg = 13.605692
-alpha = 1 / 137
+alpha = 1. / 137.
+print(alpha)
 h = 6.62606876e-34
 e = 1.602176462e-19
 c = 299792458
 d = 201e-12
 deg_to_rad = 2 * np.pi / 360
 
-# z = 80
-# x_1 = np.array([45.2, 45.5, 46.0])
-# x_2 = np.array([40.8, 41.2, 41.5])
+z = 80
+x_1 = np.array([45.2, 45.5, 46.0])
+x_2 = np.array([40.8, 41.2, 41.5])
 
 # z = 79
 # x_1 = np.array([45.2, 45.5, 46.0])
 # x_2 = np.array([40.4, 40.8, 41.2, 41.5])
 
-z = 29
-x_1 = np.array([45.2, 45.5, 46.0])
-x_2 = np.array([40.8, 41.2, 41.5])
+# z = 29
+# x_1 = np.array([45.2, 45.5, 46.0])
+# x_2 = np.array([40.8, 41.2, 41.5])
 
 x_1 /= 2
 x_2 /= 2
@@ -68,11 +69,12 @@ if x_2_sum > x_1_sum:
 else:
 	x_delta = x_1_sum - x_2_sum
 
-x_delta = 2000
+#x_delta = 2000
 
-sig = z - np.sqrt(((4 * 137) * np.sqrt(x_delta / rydberg) - 5 * x_delta / rydberg) * (1 + 19/32 * (alpha ** 2) * x_delta / rydberg))
+sig = z - np.sqrt((4 / alpha * np.sqrt(x_delta / rydberg) - 5 * x_delta / rydberg) * (1 + 19/32 * (alpha ** 2) * x_delta / rydberg))
 #sig = 0
 sig_err = sig * ((x_1_err + x_2_err) / x_delta)
+sig_error_krass = (- 95 * (alpha ** 3) * x_delta ** (3 / 2) + 57 * (alpha ** 2) * np.sqrt(rydberg) * x_delta - 80 * alpha * rydberg * np.sqrt(x_delta) + 32 * (rydberg ** (3/2)))/(4 * np.sqrt(2) * alpha * (rydberg ** 2) * np.sqrt(x_delta) * np.sqrt((4 * np.sqrt(x_delta))/(alpha * np.sqrt(rydberg)) - (5 * x_delta)/rydberg) * np.sqrt((19 * (alpha ** 2) * x_delta)/rydberg + 32)) * np.sqrt(x_1_err ** 2 + x_2_err ** 2)
 
 print('Mittelwert w1: ' + str(x_1_quer))
 print('Mittelwert w2: ' + str(x_2_quer))
@@ -81,3 +83,6 @@ print('Fehler auf E_1_Mittel: ' + str(x_1_err))
 print('Fehler auf E_2_Mittel: ' + str(x_2_err))
 print('simga: ' + str(sig))
 print('Fehler auf sigma: ' + str(sig_err))
+print('Krasser Fehler auf sigma: ' + str(sig_error_krass))
+
+
