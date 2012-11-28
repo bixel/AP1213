79b6897fc94189aa742a0cb39dd7d6bf4f844f2c
bixel <kevin.heinicke@gmail.com>
2012-11-28 17:26:06 +0100

    Korrektur  hoffentlich pusht du :D

--- V702_Aktivierung/data/fit_linear.py
+++ V702_Aktivierung/data/fit_linear.py
@@ -5,7 +5,7 @@ from scipy.optimize import curve_fit
 import datetime as dt
 import pylab
 
-data = 'rhodium'
+data = 'indium'
 
 if data == 'rhodium':
 	# Graph fuer Rhodium
@@ -13,11 +13,10 @@ if data == 'rhodium':
 
 	x *= 12
 
-	N_Dunkel = 256 / 900 * 12
-	y -= N_Dunkel
+	N_Dunkel = 256. / 900. * 12
 
-	y_splitted = np.split(y, [19, 21, 40])
-	x_splitted = np.split(x, [19, 21, 40])
+	y_splitted = np.split(y, [18, 26, 40])
+	x_splitted = np.split(x, [18, 26, 40])
 
 	y1 = y_splitted[0]
 	x1 = x_splitted[0]
@@ -25,13 +24,20 @@ if data == 'rhodium':
 	y2 = y_splitted[2]
 	x2 = x_splitted[2]
 
+	y1_error_arr = np.array([np.log(y1) - np.log(y1 - np.sqrt(y1 + N_Dunkel)), np.log(y1 + np.sqrt(y1 + N_Dunkel)) - np.log(y1)])
+	y2_error_arr = np.array([np.log(y2) - np.log(y2 - np.sqrt(y2 + N_Dunkel)), np.log(y2 + np.sqrt(y2 + N_Dunkel)) - np.log(y2)])
+
+	y -= N_Dunkel
+	y1 -= N_Dunkel
+	y2 -= N_Dunkel
+
 	y = np.log(y)
 	#plt.errorbar(x, y, yerr = 1 / np.sqrt(y ** 2 + N_Dunkel), fmt = 'kx')
 
 	x1_theorie = np.arange(0, 230.1, .2)
-	x1_theorie_punkte = np.arange(230, 370.1, .2)
-	x2_theorie = np.arange(260, 500.1, .2)
-	x2_theorie_punkte = np.arange(0, 260.1, .2)
+	x1_theorie_punkte = np.arange(230, 395.1, .2)
+	x2_theorie = np.arange(310, 500.1, .2)
+	x2_theorie_punkte = np.arange(0, 310.1, .2)
 
 	m_vorschlag = .01
 	n_0_vorschlag = 3000
@@ -61,15 +67,15 @@ if data == 'rhodium':
 	# n_0_vorschlag = 1.0
 
 	plt.xlabel(r'$t\,[\mathrm{s}]$')
-	plt.ylabel(r'$\mathrm{Zerf\"alle}\,[\mathrm{ln}(1 / 12\mathrm{s})]$')
+	plt.ylabel(r'$\mathrm{logarithmierte \, Zerf\"alle}$')
 
 	gerade_1, = plt.plot(x1_theorie, funk(x1_theorie, p[0], p[1]), 'r-')
 	plt.plot(x1_theorie_punkte, funk(x1_theorie_punkte, p[0], p[1]), 'r--')
-	werte_1 = plt.errorbar(x1, y1, yerr = np.sqrt(y1 + N_Dunkel) / (y1 - N_Dunkel), fmt = 'gx')
+	werte_1 = plt.errorbar(x1, y1, yerr = y1_error_arr, fmt = 'gx')
 
 	gerade_2, = plt.plot(x2_theorie, funk(x2_theorie, p2[0], p2[1]), 'b-')
 	plt.plot(x2_theorie_punkte, funk(x2_theorie_punkte, p2[0], p2[1]), 'b--')
-	werte_2 = plt.errorbar(x2, y2, yerr = np.sqrt(y2 + N_Dunkel) / (y2 - N_Dunkel), fmt = 'yx')
+	werte_2 = plt.errorbar(x2, y2, yerr = y2_error_arr, fmt = 'yx')
 
 	plt.legend((werte_1, gerade_1, werte_2, gerade_2), (r'$\ln{\left(N_{\Delta t} - N_{\Delta , l} - N_0 \right)}$', r'$\mathrm{Regressionsgerade\ 1}$', r'$\ln{\left( N_{\Delta t} - N_0 \right)}$', r'$\mathrm{Regressionsgerade\ 2}$'))
 
@@ -88,31 +94,37 @@ elif data == 'indium':
 	x, y = np.genfromtxt('Indium_240s.txt', unpack = True)
 
 	x *= 240
+	N_Dunkel = 256. / 900. * 240
+
+	for val in y:
+		print(round(val - N_Dunkel))
 
 	y_error = np.sqrt(y)
-
-	N_Dunkel = 256 / 900 * 240
+	y_error_arr = np.array([np.log(y) - np.log(y - np.sqrt(y + N_Dunkel)), np.log(y + np.sqrt(y + N_Dunkel)) - np.log(y)])
 	y -= N_Dunkel
 
 	y = np.log(y)
 
+	print(y_error_arr)
+
 	x_theorie = np.arange(0, 4001, 1)
 
 	plt.xlabel(r'$t\,[\mathrm{s}]$')
-	plt.ylabel(r'$\mathrm{Zerf\"alle}\,[1 / 240\mathrm{s}]$')
+	plt.ylabel(r'$\mathrm{logarithmierte \, Zerf\"alle}$')
 
 	m_vorschlag = np.log(2) / 60
 	n_0_vorschlag = 50000
+
 	vor_werte = np.array([m_vorschlag, n_0_vorschlag])
 
 	funk = lambda t, m, n_0: np.log(n_0 * (1 - np.e ** (-m * 240))) - m * t
 
-	p, cov = curve_fit(funk, x, y, p0 = vor_werte, sigma = y_error, maxfev = 1000)
+	p, cov = curve_fit(funk, x, y, p0 = vor_werte, maxfev = 1000)
 	print(p)
 	print(cov)
 
 	gerade, = plt.plot(x_theorie, funk(x_theorie, p[0], p[1]), 'r-')
-	werte = plt.errorbar(x, y, yerr = np.sqrt(y + N_Dunkel) / (y - N_Dunkel), fmt = 'kx')
+	werte = plt.errorbar(x, y, yerr = y_error_arr, fmt = 'kx')
 	plt.legend((gerade, werte), (r'$\mathrm{Regressionsgerade}$', r'$\ln{\left( N_{\Delta t} - N_0 \right)}$'))
 
 	t_halb = np.log(2) / p[0]
@@ -127,5 +139,5 @@ fig = plt.gcf() #Gibt Referent auf die aktuelle Figur - "get current figure"
 fig.set_size_inches(9, 6)
 
 plt.grid(which = 'both')
-plt.savefig('../img/graph_' + data + '_linearisiert.png', bbox_inches='tight')
+plt.savefig('../img/graph_' + data + '_linearisiert.pdf', bbox_inches='tight')
 plt.clf()
