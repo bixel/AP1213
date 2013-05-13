210587bf1e95cf34566696c8eb141a126a3a17b1
bixel <kevin.heinicke@gmail.com>
2013-05-13 11:15:33 +0200

    blaaaaaaaa

--- V301_Innenwiederstand/data/plot.py
+++ V301_Innenwiederstand/data/plot.py
@@ -76,7 +76,6 @@ for messung in messungsArray:
 		Rtheorie = np.arange(np.min(R), np.max(R), Rstep)
 		Rtheorie = Rtheorie[::-1]
 
-		#plotFunction = lambda i: - i**2 * koeffizienten[1] + i * koeffizienten[0]
 		plotFunction = lambda ra: (koeffizienten[0] ** 2 * ra) / (ra + koeffizienten[1]) ** 2
 
 		plt.errorbar(R, P, xerr = Rerror, yerr = Perror, fmt = "ko")
